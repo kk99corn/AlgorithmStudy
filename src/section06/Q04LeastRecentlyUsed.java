44962f36a0ad9dcc0f36fbf098739bed4180ade3
99 <hahahoho5915@gmail.com>
2024-11-27 18:12:32 +0900

    Section06_04 LRU
    url: https://cote.inflearn.com/contest/10/problem/06-04

--- src/section06/Q04LeastRecentlyUsed.java
+++ src/section06/Q04LeastRecentlyUsed.java
@@ -27,6 +27,34 @@ public class Q04LeastRecentlyUsed {
         }
     }
 
+    public void stdSolution(int s, int n, int[] arr) {
+        int[] cache = new int[s];
+        for (int x : arr) {
+            int pos = -1;
+            for (int i = 0; i < s; i++) {
+                if (x == cache[i]) {
+                    pos = i;
+                }
+            }
+
+            if (pos == -1) {
+                for (int i = s - 1; i >= 1; i--) {
+                    cache[i] = cache[i - 1];
+                }
+                cache[0] = x;
+            } else {
+                for (int i = pos; i >= 1; i--) {
+                    cache[i] = cache[i - 1];
+                }
+                cache[0] = x;
+            }
+        }
+
+        for (int i : cache) {
+            System.out.print(i + " ");
+        }
+    }
+
     public static void main(String[] args) {
         Q04LeastRecentlyUsed t = new Q04LeastRecentlyUsed();
         Scanner sc = new Scanner(System.in);
@@ -39,6 +67,8 @@ public class Q04LeastRecentlyUsed {
         }
 
         t.solution(s, n, arr);
+        System.out.println();
+        t.stdSolution(s, n, arr);
         sc.close();
     }
 }

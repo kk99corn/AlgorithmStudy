72712c88ef254ed942d8d2ab81b06b35b9677b39
99 <hahahoho5915@gmail.com>
2024-12-18 17:43:46 +0900

    Section07 DFS, BFS
    url: https://cote.inflearn.com/contest/10/problem/07

---
+++ src/section07/Q05DFS.java
@@ -0,0 +1,45 @@
+package section07;
+
+import java.util.*;
+
+/*
+section: 06-05
+title: 이진트리순회(DFS)
+*/
+
+class Node {
+    int data;
+    Node lt;
+    Node rt;
+
+    public Node(int n) {
+        data = n;
+    }
+}
+
+public class Q05DFS {
+    Node root;
+
+    public void DFS(Node root) {
+        if (root == null) {
+            return;
+        } else {
+            DFS(root.lt);
+            DFS(root.rt);
+            System.out.print(root.data + " ");
+        }
+    }
+
+    public static void main(String[] args) {
+        Q05DFS t = new Q05DFS();
+        t.root = new Node(1);
+        t.root.lt = new Node(2);
+        t.root.rt = new Node(3);
+        t.root.lt.lt = new Node(4);
+        t.root.lt.rt = new Node(5);
+        t.root.rt.lt = new Node(6);
+        t.root.rt.rt = new Node(7);
+
+        t.DFS(t.root);
+    }
+}
---
+++ src/section07/Q08Calf.java
@@ -0,0 +1,54 @@
+package section07;
+
+import java.util.*;
+
+/*
+section: 06-05
+title: 이진트리순회(DFS)
+*/
+
+public class Q08Calf {
+
+    int answer = 0;
+    int[] dis = { 1, -1, 5 };
+    int[] check;
+    Queue<Integer> q = new LinkedList<>();
+
+    public int solution(int s, int e) {
+        check = new int[10001];
+        check[s] = 1;
+        q.offer(s);
+        int level = 0;
+        while (!q.isEmpty()) {
+            int size = q.size();
+            for (int i = 0; i < size; i++) {
+                int x = q.poll();
+                if (x == e) {
+                    return level;
+                }
+                for (int k : dis) {
+                    if (x + k == e)
+                        return level + 1;
+                    if (x + k > 0 && x + k <= 10000 && check[x + k] == 0) {
+                        check[x + k] = 1;
+                        q.offer(x + k);
+                    }
+                }
+            }
+            level++;
+        }
+
+        return answer;
+    }
+
+    public static void main(String[] args) {
+        Q08Calf t = new Q08Calf();
+
+        Scanner sc = new Scanner(System.in);
+        int s = sc.nextInt();
+        int e = sc.nextInt();
+
+        System.out.println(t.solution(s, e));
+        sc.close();
+    }
+}
